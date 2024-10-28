7d1931f613a3abfc82f47519f49dfa5ec300cc22
rakesh333-web <85956046+rakesh333-web@users.noreply.github.com>
2024-10-28 10:39:10 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ from chromadb.config import Settings
 load_dotenv()
 
 # Define the folder for storing database
-PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY')
+PERSIST_DIRECTORY = './vectorstore'
 
 # Define the Chroma settings
 CHROMA_SETTINGS = Settings(
